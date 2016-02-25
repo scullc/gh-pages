368d7da9e01cb67cf94bf1fc4e3837b29d079001
scully80@gmail.com <scullchr@augspdlamp02.wisbis.root.domain>
2016-02-25 15:02:39 +1100

    d

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,15 @@
-document.writeln("test");
+// Create myObject. It has a value and an increment
+// method. The increment method takes an optional
+// parameter. If the argument is not a number, then 1
+// is used as the default.
+var myObject = {
+value: 0,
+increment: function (inc) {
+this.value += typeof inc === 'number' ? inc : 1;
+}
+};
+myObject.increment( );
+document.writeln(myObject.value); // 1
+myObject.increment(2);
+document.writeln(myObject.value); // 3
 
